c4e66705db4f9604380586c7abcc800ed9fed977
Takashi IIGUNI <iiguni.tks@gmail.com>
2020-07-13 00:03:43 +0900

    Impl health_check for GET /

--- hound/src/main.rs
+++ hound/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+
+mod api;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(api::health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
